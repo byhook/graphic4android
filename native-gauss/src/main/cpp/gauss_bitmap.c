ef2915ec94680229fa928f18510a8fce103e7519
Andy <byhook@163.com>
2018-11-22 16:23:49 +0800

    native实现对bitmap高斯模糊

--- native-gauss/src/main/cpp/gauss_bitmap.c
+++ native-gauss/src/main/cpp/gauss_bitmap.c
@@ -11,7 +11,7 @@
 
 #include <android/log.h>
 
-#define LOG_TAG    "HelloFFmpeg"
+#define LOG_TAG    "GaussBitmap"
 #define LOGE(format, ...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, format, ##__VA_ARGS__)
 #define LOGI(format, ...)  __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, format, ##__VA_ARGS__)
 #else
